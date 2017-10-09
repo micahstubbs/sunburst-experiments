2d5857ecde52fa204eeef249fb06bcbcf65e1037
Micah Stubbs <micah.stubbs@gmail.com>
2017-10-09 11:04:59 -0700

    add nice sunlight foundation colors

--- 01/README.md
+++ 01/README.md
@@ -1,5 +1,7 @@
 an iteration with a different color palette
 
+here we use this 5-color qualitative color scheme from http://colorbrewer2.org/, also used in [ES2015 Sequences Sunburst](https://bl.ocks.org/micahstubbs/428036f0f948a5c2b95f8a4b91931ee6/d904583607a6ff3f572453437835eab6e1cb1c8e)
+
 a fork of [Zoomable Sunburst with Labels](https://bl.ocks.org/vasturiano/12da9071095fbd4df434e60d52d2d58d) from [@vastur](https://twitter.com/vastur)
 
 ---
--- 01/vis.js
+++ 01/vis.js
@@ -11,7 +11,53 @@ const x = d3
 
 const y = d3.scaleSqrt().range([maxRadius * 0.1, maxRadius]);
 
-const color = d3.scaleOrdinal(d3.schemeCategory20);
+// sunlight style guide network colors
+// https://github.com/amycesal/dataviz-style-guide/blob/master/Sunlight-StyleGuide-DataViz.pdf
+const dark = [
+  '#B08B12',
+  '#BA5F06',
+  '#8C3B00',
+  '#6D191B',
+  '#842854',
+  '#5F7186',
+  '#193556',
+  '#137B80',
+  '#144847',
+  '#254E00'
+];
+
+const mid = [
+  '#E3BA22',
+  '#E58429',
+  '#BD2D28',
+  '#D15A86',
+  '#8E6C8A',
+  '#6B99A1',
+  '#42A5B3',
+  '#0F8C79',
+  '#6BBBA1',
+  '#5C8100'
+];
+
+const light = [
+  '#F2DA57',
+  '#F6B656',
+  '#E25A42',
+  '#DCBDCF',
+  '#B396AD',
+  '#B0CBDB',
+  '#33B6D0',
+  '#7ABFCC',
+  '#C8D7A1',
+  '#A0B700'
+];
+
+const palettes = [light, mid, dark];
+const lightGreenFirstPalette = palettes
+  .map(d => d.reverse())
+  .reduce((a, b) => a.concat(b));
+
+const color = d3.scaleOrdinal(lightGreenFirstPalette);
 
 const partition = d3.partition();
 
